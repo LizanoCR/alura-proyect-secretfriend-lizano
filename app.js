1cb354db103142d2fc6faa4558f3c526839cda53
Daniel <danylizano2001@gmail.com>
2025-09-03 13:39:28 -0600

    Funciones completadas

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ function asignarTexto(elemento, texto) {
 
 /*
 Vamos a primero a empezar a interactuar primero con el boton inciar de "Añadir", por lo que vamos a crear la Fusion "AgregarAmigo"
+y vamos a tomar los valores que entren llamandolos con "document.getElementById" esto ayuda a poder interactuar con los elemento en el JS.
+Se crea una lista para guardar estos elementos haciendo cada una de su verificaciones.
 */
 function AgregarFriend() {
   //getElementById == Obtener el elemento
@@ -28,5 +30,18 @@ function AgregarFriend() {
 
 function mostrarLista() {
   let lista = document.getElementById("listaAmigos"); //Convertimos el ID en una variable en JS para poder interactuar con el codigo
-  lista.innerHTML = ""; //Limpia la lista antes de volver a imprimirtla
+  lista.innerHTML = ""; //Limpia la lista antes de volver a imprimirtla en pantalla
+
+  nombresLista.forEach((nombresLista) => {
+    //forEach = recorrer listas de forma más limpias
+    let list2 = document.createElement("list2");
+    list2.textContent = Amigo;
+    nombresLista.appendChild(list2);
+  });
+}
+
+function sorteoAmigo() {
+  if (nombresLista.length === 0) {
+    alert("No hay amigos en la lista para sortear");
+  }
 }
