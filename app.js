126fe8b3acf64b14f4d7d4c840504b03a3ac1b8f
Daniel <danylizano2001@gmail.com>
2025-09-03 13:50:58 -0600

    Reporitorio listo

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ function asignarTexto(elemento, texto) {
 /*
 Vamos a primero a empezar a interactuar primero con el boton inciar de "Añadir", por lo que vamos a crear la Fusion "AgregarAmigo"
 y vamos a tomar los valores que entren llamandolos con "document.getElementById" esto ayuda a poder interactuar con los elemento en el JS.
-Se crea una lista para guardar estos elementos haciendo cada una de su verificaciones.
+Se crea una lista para guardar estos elementos, haciendo cada una de su verificaciones.
 */
 function AgregarFriend() {
   //getElementById == Obtener el elemento
   let nameFriend = document.getElementById("valorAmigo").value;
-  let nombre = nameFriend.value.trim();
+  let nombre = nameFriend.value.trim(); //trim = elimina espacios en blanco al inicio y al final MUY IMPORTANTE
 
   if (nombre === "") {
     alert("Por favor, ingrese un nombre válido.");
     return;
   }
 
-  amigos.push(nombre); //Agrega el nombre a la Array
+  nombresLista.push(nombre); //Agrega el nombre a la Array
   mostrarLista(); //Actualiza la lista
   nameFriend.value = ""; //Limpia el campo de texto inmediatamente
 
@@ -36,7 +36,7 @@ function mostrarLista() {
     //forEach = recorrer listas de forma más limpias
     let list2 = document.createElement("list2");
     list2.textContent = Amigo;
-    nombresLista.appendChild(list2);
+    nombresLista.appendChild(list2); //Lo agrega como si fuera un hijo ya que esta escrito en html (h1(Lista de amigos) : h2(Carlos) : h2(Isaac))
   });
 }
 
@@ -44,4 +44,10 @@ function sorteoAmigo() {
   if (nombresLista.length === 0) {
     alert("No hay amigos en la lista para sortear");
   }
+
+  let eleccionAleatoria = Math.floor(Math.random() * nombresLista.length);
+  let Ganador = nombresLista[eleccionAleatoria];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li> El amigo secreto es: <strong>${Ganador}</strong></li>`;
 }
