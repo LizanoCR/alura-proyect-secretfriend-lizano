2e58637aeae4244c5eca77b0366c86bb80084a79
Daniel <danylizano2001@gmail.com>
2025-09-02 19:59:13 -0600

    Fuction AgregarFriend Listo!

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresLista = []; //Se almacena los nombres
+
+function asignarTexto(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+/*
+Vamos a primero a empezar a interactuar primero con el boton inciar de "Añadir", por lo que vamos a crear la Fusion "AgregarAmigo"
+*/
+function AgregarFriend() {
+  //getElementById == Obtener el elemento
+  let nameFriend = document.getElementById("valorAmigo").value;
+  let nombre = nameFriend.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, ingrese un nombre válido.");
+    return;
+  }
+
+  amigos.push(nombre); //Agrega el nombre a la Array
+  mostrarLista(); //Actualiza la lista
+  nameFriend.value = ""; //Limpia el campo de texto inmediatamente
+
+  return;
+}
+
+function mostrarLista() {
+  let lista = document.getElementById("listaAmigos"); //Convertimos el ID en una variable en JS para poder interactuar con el codigo
+  lista.innerHTML = ""; //Limpia la lista antes de volver a imprimirtla
+}
